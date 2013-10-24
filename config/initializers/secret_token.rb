607ed3968ce78caaf57a45c2d28e3f9a01760318
Jhonatan Castro <jhtan@jhtan.com>
2013-10-24 19:03:36 -0400

    Initial Commit of Wanted @_@

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wanted::Application.config.secret_key_base = '5f81f648719486b7bed8200a6e5d6eae45a5d77cbccc1533cafa9c4b104f13eada4a34ace00d635f4723e455194ec135478a2f0ac69dd5ee959ffce530a29b8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wanted::Application.config.session_store :cookie_store, key: '_Wanted_session'
